9767359286012a8fb7e1eecc7edd2575d6f22690
andresVillarreal988 <andres.villareal@miru.com.mx>
2024-05-03 13:15:09 -0600

    Changes to portfolio.

--- src/app/pages/portfolio/portfolio.component.ts
+++ src/app/pages/portfolio/portfolio.component.ts
@@ -26,7 +26,7 @@ export class PortfolioComponent implements OnInit {
       "https://ocr-miru-dev.uc.r.appspot.com/",
       "Developed a comprehensive web platform aimed at facilitating the real estate buying and selling process, enhancing user experience.",
       "Implemented a dynamic listing system that allows users to filter, search, and manage property listings efficiently, increasing user engagement and satisfaction.",
-      "Designed back end models, services, controllers, repositories and mapped the objects to Postgres tables by using Hibernate and JPA",
+      "Designed back end models, services, controllers, repositories and mapped the objects to Postgres tables by using Hibernate and JPA.",
       "Collaborated closely with a cross-functional team to deliver the main functionalities of the site. Led the weekly team meeting for the dev team.",
       "Utilized technologies such as Angular for the frontend to create interactive user interfaces, Spring for backend services.",
       "Led the deployment and continuous integration of the project using Docker and Google Cloud, significantly reducing downtime and improving the deployment process."
@@ -40,11 +40,12 @@ export class PortfolioComponent implements OnInit {
       startDate: 'August 2023',
       endDate: 'February 2024',
       responsibilities: [
-        'Development of a customer data updating system for Banamex',
-        'Development of an Autochecker within the system to verify that the user updating the data is a person with authorization by the bank',
-        'Creation and management of views for users and front end functionalities',
-        'Creating flowcharts for different User Stories',
-        'Technologies used: Java, C#, Struts, MVC, DAO, HTML5, CSS3, JavaScript, XML, WebLogic, jQuery. Softtek Courses'
+        'Developed a secure front-end form for authenticated users from Banamex to update their passwords, featuring fields for the old password, new password, and password confirmation.',
+        'Implemented stringent password validation rules to ensure compliance with security standards, enhancing system integrity.',
+        'Engineered an Autochecker feature to verify the identity of users attempting to make updates, thereby bolstering security measures.',
+        'Designed user interfaces and managed views to optimize user experience and facilitate seamless data updates.',
+        'Created detailed flowcharts for User Stories, aiding in the visualization and planning of development tasks.',
+        'Utilized a diverse technology stack, including Java, C#, Struts, MVC, DAO, HTML5, CSS3, JavaScript, XML, WebLogic, jQuery. Softtek Courses'
         // ... more responsibilities
       ],
     },
@@ -55,13 +56,12 @@ export class PortfolioComponent implements OnInit {
           startDate: 'Nov 2022',
           endDate: 'Jul 2023',
           responsibilities: [
-            'Development of a sales system, where the product catalogs, customer module and suppliers modules were developed',
-            'Development of a module for home delivery of products',
-            'Maintenance of the customer and supplier catalog',
-            'Creating and consuming stored queries and procedures',
-            'Application of functionality testing and error correction',
-            'Database management and creation in SQL Server',
-            'Technologies used: Java, JSF, MVC, DAO, Facade Front Controller, HTML5, CSS3, JavaScript, XML, WebServices, Oracle 11g'
+            'Led the development of a comprehensive sales system encompassing product catalogs, customer, and supplier modules.',
+            'Engineered a home delivery module to expand the service offerings and enhance customer convenience.',
+            'Maintained and updated the customer and supplier catalogs, ensuring accuracy and reliability.',
+            'Managed database operations, including creating and executing stored queries and procedures in SQL Server.',
+            'Conducted functionality testing and debugging to ensure robust application performance.',
+            'Utilized technologies such as Java, JSF, MVC, DAO, Facade Front Controller, HTML5, CSS3, JavaScript, XML, WebServices, Oracle 11g'
             // ... more responsibilities
           ],
         },
@@ -72,13 +72,13 @@ export class PortfolioComponent implements OnInit {
            startDate: 'July 2021',
            endDate: 'November 2022',
            responsibilities: [
-             'Development of a system for the control of customer records for the bank Santander',
-             'Development of a module for dealing with incidents in the different departments of the company',
-             'Improvements were made to the front end of the warehouse system',
-             'Preventive and corrective maintenance of the system modules of the production lines',
-             'Application of functionality testing and error correction',
-             'Updating website',
-             'Technologies used: Java, Spring, MVC, DAO, Front Controller, HTML5, CSS3, JavaScript, XML, WebServices, Oracle 11g, Ajax'
+             'Developed a sophisticated system for managing customer records for Santander, optimizing operational efficiency.',
+             'Created a specialized module to handle incidents across various company departments, enhancing response capabilities.',
+             'Improved the front-end functionality of the warehouse management system, increasing user engagement.',
+             'Performed preventive and corrective maintenance on production line system modules, ensuring continuous operation.',
+             'Implemented comprehensive testing and debugging procedures to maintain system integrity and functionality.',
+             'Updated the corporate website, enhancing user experience and information accessibility.',
+             'Integrated technologies including Java, Spring, MVC, DAO, Front Controller, HTML5, CSS3, JavaScript, XML, WebServices, Oracle 11g, Ajax'
              // ... more responsibilities
            ],
          },
